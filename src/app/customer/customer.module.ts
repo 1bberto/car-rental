8baea5f336c28c658e26083434a2588d3e7726e5
1bberto <humberto_henrique1@live.com>
2019-11-06 16:41:35 -0300

    2 files modified, 4 added

--- src/app/customer/customer.module.ts
+++ src/app/customer/customer.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import  { CustomerComponent } from './customer/customer.component';
+
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [CustomerComponent]
 })
 export class CustomerModule { }

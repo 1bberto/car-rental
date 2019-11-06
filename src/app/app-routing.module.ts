a104b7826261a667a1d533cb6e7bfe3fe0d69f0c
1bberto <humberto_henrique1@live.com>
2019-11-06 16:04:59 -0300

    1 file modified, 5 added

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+
+import { HomeModule } from "./home/home.module";
+import { LoginModule } from "./login/login.module";
+
+const routes: Routes = [
+  {
+    path: "",
+    loadChildren: () => HomeModule
+  },
+  {
+    path: "login",
+    loadChildren: () => LoginModule
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
